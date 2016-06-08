43788007b11e7433ba898ec5f57d5447cb1158ef
Aurick Qiao <qiao@aurick.net>
2016-06-08 18:32:55 -0400

    add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ todo_include_todos = False
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-# html_favicon = None
+html_favicon = '_images/favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
